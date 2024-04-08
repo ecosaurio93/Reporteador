913a75339480706f3ec20392be7b03d217b290b9
Enrique Correa Ojeda <enrique-m3@hotmail.com>
2024-04-08 00:11:16 -0600

    reporteador con reportviewer

--- RDLC/DataSet1.cs
+++ RDLC/DataSet1.cs
@@ -1,3 +1,7 @@
 ﻿namespace RDLC
 {
 }
+
+namespace RDLC
+{
+}
--- RDLC/Program.cs
+++ RDLC/Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace RDLC
 {
     internal static class Program
@@ -8,10 +10,9 @@ namespace RDLC
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            Debug.WriteLine("Se setean los valores de el data set");
         }
     }
 }
